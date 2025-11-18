bb29a45b49be66a428c9431653052b584800db11
bellazhouyiyi-afk <bellazhouyiyi@gmail.com>
2025-11-18 03:36:39 +0000

    1

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 # flask
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-        r = request.form.get("q")
-        return(render_template("index.html"))
+       return(render_template("index.html"))
 
 @app.route("/main",methods=["GET","POST"])
 def main():
-        return(render_template("main.html"))
+       return(render_template("main.html"))
 
 @app.route("/dbs",methods=["GET","POST"])
 def dbs():
